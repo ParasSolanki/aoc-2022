0c912286ce3b2d35c672a8b8230ad924c5043408
parasSolanki <parassolanki151@gmail.com>
2022-12-06 21:27:38 +0530

    add: day 6 part 2 solution

---
+++ day-06/2.ts
@@ -0,0 +1,19 @@
+const input = await Deno.readTextFile('./input.txt')
+
+const str: string[] = []
+let index = 0
+for (const [idx, ch] of input.split('').entries()) {
+  if (str.length === 14) {
+    index = idx
+    break
+  }
+  // has in array
+  if (str.includes(ch)) {
+    // check index from remove previous character from list
+    const index = str.findIndex((c) => c === ch)
+    str.splice(0, index + 1)
+  }
+  str.push(ch)
+}
+
+console.log(index) // 2421
